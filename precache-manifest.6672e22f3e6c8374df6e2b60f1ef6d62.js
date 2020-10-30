4047fb99c925cd1905d9a960b4ff2970cca7d9ef
harika <harikabhaskaram31@gmail.com>
2020-10-30 22:12:01 +0530

    Updates

---
+++ precache-manifest.6672e22f3e6c8374df6e2b60f1ef6d62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5280295e77c19df1f8fa715d6ca6a8",
+    "url": "/GPRC/index.html"
+  },
+  {
+    "revision": "4b6aecaec38f1a5447da",
+    "url": "/GPRC/static/css/main.eb4dc68a.chunk.css"
+  },
+  {
+    "revision": "979e260eefb9e789697d",
+    "url": "/GPRC/static/js/2.23278d72.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/GPRC/static/js/2.23278d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6aecaec38f1a5447da",
+    "url": "/GPRC/static/js/main.b70effba.chunk.js"
+  },
+  {
+    "revision": "e946cd5e53ad32cd26f6",
+    "url": "/GPRC/static/js/runtime-main.6eb66492.js"
+  },
+  {
+    "revision": "afe0f4b92ce45910515d18924a33450c",
+    "url": "/GPRC/static/media/gprc-1.afe0f4b9.svg"
+  },
+  {
+    "revision": "e64a5ee6191008b978ee0391570e1eee",
+    "url": "/GPRC/static/media/image1.e64a5ee6.png"
+  },
+  {
+    "revision": "c6f003d326f30860903e4d0c912288b4",
+    "url": "/GPRC/static/media/image2.c6f003d3.png"
+  }
+]);
